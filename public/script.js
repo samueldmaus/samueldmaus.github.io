090e2bc5da367e41d6b67353708c03223fb95e76
Sam Maus <samueldmaus@gmail.com>
2020-11-15 23:54:36 -0600

    added animation to name

--- public/script.js
+++ public/script.js
@@ -21,13 +21,31 @@ tl.add({
     width: '100%',
     boxShadow: '0 10px 30px 5px rgba(0, 0, 0, 0.19), 0 10px 10px 5px rgba(0, 0, 0, 0.23)',
     delay: anime.stagger(100),
-
 })
 
- tl.add({
+tl.add({
     targets: '.project_div',
     width: '95%',
     delay: anime.stagger(100)
+})
+
+
+anime({
+    targets: '#logo path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutQuad',
+    duration: 2000,
+    delay: 1000,
+    direction: 'alternate',
+    loop: true
+})
+
+anime({
+        targets: '.underline',
+        rotate: 360,
+        delay: 5000,
+        loop: true,
+        
 })
 
 // PROJECTS //
